f01f365b4c3b76e4c11bbefa75a221a7ac4906a6
Alex Latham <alexlthm@gmail.com>
2020-02-10 22:16:55 -0700

    Add setup test

--- test/worldcup_test.rb
+++ test/worldcup_test.rb
@@ -6,19 +6,20 @@ require './lib/worldcup'
 
 class WorldCupTest < Minitest::Test
 
+  def setup
+    @france = Team.new("France")
+    @mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    @pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+    @france.add_player(@mbappe)
+    @france.add_player(@pogba)
+    @croatia = Team.new("Croatia")
+    @modric = Player.new({name: "Luka Modric", position: "midfielder"})
+    @vida = Player.new({name: "Domagoj Vida", position: "defender"})
+    @croatia.add_player(@modric)
+    @croatia.add_player(@vida)
+    @world_cup = WorldCup.new(2018, [@france, @croatia])
+  end
   def test_it_can_exist
-    france = Team.new("France")
-    mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
-    pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
-    france.add_player(mbappe)
-    france.add_player(pogba)
-    croatia = Team.new("Croatia")
-    modric = Player.new({name: "Luka Modric", position: "midfielder"})
-    vida = Player.new({name: "Domagoj Vida", position: "defender"})
-    croatia.add_player(modric)
-    croatia.add_player(vida)
-    world_cup = WorldCup.new(2018, [france, croatia])
-
-    assert_instance_of WorldCup, world_cup
+    assert_instance_of WorldCup, @world_cup
   end
 end
